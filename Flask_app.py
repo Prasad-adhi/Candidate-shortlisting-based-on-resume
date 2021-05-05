8684e27f947038426a1fe5fe29ca90f9baf96d0a
Prasad-adhi <68509452+Prasad-adhi@users.noreply.github.com>
2021-05-05 19:52:12 +0530

    Created with a database

--- Flask_app.py
+++ Flask_app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template, request,jsonify
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import docx2txt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from Scanner import similarity
+import sqlite3
 
-app = Flask(__name__)
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///./Candidates.db"
+db = SQLAlchemy(app)
+class Candidate(db.Model):
+    name=db.Column(db.String(300))
+    contactno=db.Column(db.Numeric(10))
+    emailid=db.Column(db.String(50),primary_key=True)
+    filename=db.Column(db.String(50))
 
 @app.route('/')
 def upload():
@@ -16,10 +25,23 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
-      f1 = open("database.txt", "a")
-      f1.write(f.filename)
-      f1.write("\n")
-      f1.close()
+      name=request.form['cname']
+      number=request.form['c-cno']
+      email=request.form['cemail']
+      query="INSERT INTO Candidate(name,contactno,emailid,filename) VALUES ("+"'"+name+"',"+"'"+str(number)+"',"+"'"+email+"',"+"'"+f.filename+"')"
+      conn=None
+      try:
+         conn = sqlite3.connect("Candidates.db")
+         cur = conn.cursor()
+         cur.execute(query)
+         conn.commit()
+         cur.close
+      except:
+         print()
+      finally:
+         if (conn):
+            conn.close()
+      
       return 'file uploaded successfully'
 
 @app.route('/admin')
@@ -28,8 +50,8 @@ def admin():
 
 @app.route('/Result')
 def Result():
-    output = similarity()
-    return jsonify(output)
+   output = similarity()
+   return jsonify(output)
 		
 if __name__ == '__main__':
    app.run(debug = True)
--- Scanner.py
+++ Scanner.py
@@ -1,29 +1,46 @@
 import docx2txt
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import sqlite3
 
 def similarity():
-    f = open("database.txt", "r")
+    query="SELECT * FROM Candidate"
+    conn=None
+    rows=[]
     score=[]
-    names=f.readlines()
-    for line in names:
-        resume = docx2txt.process(line[:len(line)-1])
+    try:
+        conn = sqlite3.connect("Candidates.db")
+        cur = conn.cursor()
+        cur.execute(query)
+        rows = cur.fetchall()
+        conn.commit()
+        cur.close
+    except:
+        print("Error")
+    finally:
+        if (conn):
+            conn.close()
+    for line in rows:
+        resume = docx2txt.process(line[3])
         job_desc = docx2txt.process("job_description.docx")
         text = [resume,job_desc]
         cv=CountVectorizer()
         count_matrix=cv.fit_transform(text)
         score.append(cosine_similarity(count_matrix)[0][1])
-    for i in range(0,len(names)-1):
-        for j in range(0,len(names)-i-1):
+    for i in range(0,len(rows)-1):
+        for j in range(0,len(rows)-i-1):
             if(score[j]<score[j+1]):
                 temp1=score[j]
                 score[j]=score[j+1]
                 score[j+1]=temp1
-                temp2=names[j]
-                names[j]=names[j+1]
-                names[j+1]=temp2
+                temp2=rows[j]
+                rows[j]=rows[j+1]
+                rows[j+1]=temp2
     output={}
-    output['Names'],output['Score']=names,score
+    output['Names'],output['Contact'],output['Email'],output['File'],output['Score']=[],[],[],[],score
+    for i in range(len(rows)):
+        output['Names'].append(rows[i][0])
+        output['Contact'].append(rows[i][1])
+        output['Email'].append(rows[i][2])
+        output['File'].append(rows[i][3])
     return output
-
-print(similarity())
