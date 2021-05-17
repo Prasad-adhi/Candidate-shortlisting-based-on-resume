66fe6da4119a8cbc123607c69a477d70e51fd583
Prasad-adhi <68509452+Prasad-adhi@users.noreply.github.com>
2021-05-17 18:17:17 +0530

    Added new paths for login and job upload

--- Flask_app.py
+++ Flask_app.py
@@ -11,6 +11,7 @@ import os
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///./Candidates.db"
+
 db = SQLAlchemy(app)
 class Candidate(db.Model):
     name=db.Column(db.String(300))
@@ -25,8 +26,9 @@ def upload():
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
+      app.config['UPLOAD_FOLDER']="./resumes"
       f = request.files['file']
-      f.save(secure_filename(f.filename))
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
       name=request.form['cname']
       number=request.form['c-cno']
       email=request.form['cemail']
@@ -44,7 +46,22 @@ def upload_file():
          if (conn):
             conn.close()
       
-      return 'file uploaded successfully'
+      return render_template('message.html')
+
+@app.route('/login')
+def login():
+   return render_template('Login.html')
+
+@app.route('/job_upload',methods=["GET","POST"])
+def job_upload():
+   if request.method == 'POST':
+      app.config['UPLOAD_FOLDER']="./jobs"
+      f = request.files['file']
+      f.filename="job_description.docx"
+      f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+      text={}
+      text["text"]="File uploaded successfully"
+      return jsonify(text)
 
 @app.route('/admin')
 def admin():
@@ -55,9 +72,10 @@ def Result():
    output = similarity()
    return jsonify(output)
 
-@app.route('/<path:filename>')
+@app.route('/resumes/<path:filename>')
 def download(filename):
-   return send_from_directory(directory="./", filename=filename)
+   return send_from_directory(directory="./resumes", filename=filename)
+
 		
 if __name__ == '__main__':
    app.run(debug = True)
